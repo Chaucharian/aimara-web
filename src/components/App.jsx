2bcaf1f99372a69cd1a5f6e9c46743fbb39de136
Agustin Salinas <chaucharian@gmail.com>
2019-04-10 14:40:03 -0300

    material-ui implemented

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Grid } from '@material-ui/core';
+
+const App = () => (
+    <Grid direction='column'>
+    <div><Button>AA</Button></div>
+    <div><Button>BB</Button></div>
+    </Grid>
+);
+
+export default App;
